a6f4e81e4276264ab9fffa2f84b7cdd68af86e91
TatchNicolas <TatchNicolas@users.noreply.github.com>
2018-07-15 16:35:28 +0900

    Fix redirection of stderr

--- gitlab-getting-started/vagrant/setup_remote_user.sh
+++ gitlab-getting-started/vagrant/setup_remote_user.sh
@@ -13,6 +13,6 @@ echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 mkdir /home/$USER/.ssh
 chown $USER:$USER /home/$USER/.ssh
 chmod 700 /home/$USER/.ssh
-curl https://github.com/$GITHUB_ID.keys > /home/$USER/.ssh/authorized_keys 2>&1
+curl https://github.com/$GITHUB_ID.keys > /home/$USER/.ssh/authorized_keys 2>/dev/null
 echo AllowUsers $USER vagrant >> /etc/ssh/sshd_config
 systemctl restart sshd
